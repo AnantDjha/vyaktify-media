f8f740fd7718b6e14a4e775e3a5ebf1f7f1114fb
Anant <anantjha0112@gmail.com>
2025-12-12 15:00:46 +0530

    all done

--- frontend--/src/home/landing-page.tsx
+++ frontend--/src/home/landing-page.tsx
@@ -5,7 +5,7 @@ export const LandingPage = () => {
     return (
         <>
             <SEO
-                title="Vyaktify Media | Digital Marketing & Web Development Agency"
+                title="Vyaktify Media | Creative Marketing & Web Development Agency"
                 description="Vyaktify Media offers premium digital marketing, web development, video production, and branding services to elevate your business."
             />
             <FrontBanner />
