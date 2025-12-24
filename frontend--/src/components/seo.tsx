78214103131fa78b06aa303c9d6d86a7f2cc3ec7
Anant <anantjha0112@gmail.com>
2025-12-24 23:50:32 +0530

    ...

--- frontend--/src/components/seo.tsx
+++ frontend--/src/components/seo.tsx
@@ -34,6 +34,20 @@ export default function SEO({
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
       <meta name="twitter:site" content="@VyaktifyMedia" />
+
+      {/* Structured Data (JSON-LD) for Google Search */}
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "Vyaktify Media",
+          "url": "https://vyaktifymedia.com",
+          "logo": "https://vyaktifymedia.com/og_logo.jpg",
+          "sameAs": [
+            "https://twitter.com/VyaktifyMedia"
+          ]
+        })}
+      </script>
     </Helmet>
   );
 }
